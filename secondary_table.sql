263a5ec51e599f945ace1b7b0ffe6596c7766947
Veronika151 <ziburova.veronika@seznam.cz>
2026-07-17 19:38:06 +0200

    Update secondary_table.sql

--- secondary_table.sql
+++ secondary_table.sql
@@ -1,17 +1,13 @@
 CREATE TABLE t_veronika_ziburova_project_SQL_secondary_final AS
-
 SELECT
     c.country,
     e.year,
     e.gdp,
     e.gini,
     e.population
-
 FROM countries c
-
 JOIN economies e
     ON e.country = c.country
-
 WHERE 
     e.year BETWEEN 2006 AND 2018
     AND c.continent = 'Europe';
@@ -19,6 +15,5 @@ WHERE
 -- =========================================================
 -- Kontrola výsledné tabulky
 -- =========================================================
-
 SELECT *
 FROM t_veronika_ziburova_project_SQL_secondary_final;
