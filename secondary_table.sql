3ba413cf316337ceeb1c3f96360c99e12eb9ee15
Veronika151 <ziburova.veronika@seznam.cz>
2026-07-17 18:09:41 +0200

    Update secondary_table.sql

--- secondary_table.sql
+++ secondary_table.sql
@@ -16,7 +16,9 @@ WHERE
     e.year BETWEEN 2006 AND 2018
     AND c.continent = 'Europe';
 
+-- =========================================================
 -- Kontrola výsledné tabulky
+-- =========================================================
 
 SELECT *
 FROM t_veronika_ziburova_project_SQL_secondary_final;
